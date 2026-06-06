96fa5c8d0951c14f3e3a570036b4c8488885c59e
RezaALfath_ <rezarezaalfath@gmail.com>
2026-06-06 20:11:03 +0700

    Create nullSafetyCondition.dart

---
+++ nullSafetyCondition.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  int? nilaiMatematika;
+
+  if (skor != null){
+    print('Nilai Matematika: $nilaiMatematika');
+  }
+
+  else{
+    print('Siswa belum ujian matematika');
+  }
+}
