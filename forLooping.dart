d4d77339352a44e81d8c292fc7c31e6b66d53833
RezaALfath_ <rezarezaalfath@gmail.com>
2026-06-06 19:52:48 +0700

    Update forLooping.dart

--- forLooping.dart
+++ forLooping.dart
@@ -1,5 +1,6 @@
-void main() {
-  for (int hitungan = 1; hitungan <= 3; hitungan++) {
-    print('Saya tidak akan telat lagi');
+void main(){
+  String nama = 'Reza';
+  for (int i = 0; i < 3; i++){
+    print(nama);
   }
 }
