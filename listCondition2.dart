d3541474baee6f10f957fb4f3d76516dff5cacf0
RezaALfath_ <rezarezaalfath@gmail.com>
2026-06-06 18:51:33 +0700

    Create listCondition2.dart

---
+++ listCondition2.dart
@@ -0,0 +1,13 @@
+void cekKamarNunggak({required List<String> statusKamar}){
+  for(int i = 0 ; i < statusKamar.length ; i++){
+    if (statusKamar[i] == 'Nunggak'){
+      print('PERINGATAN: kamar nomor ${i + 1} BELUM BAYAR!');
+    }
+  }
+}
+
+void main(){
+ List<String> dataKos = cekKamarNunggak(statusKamar: ['Lunas','Nunggak','Lunas','Nunggak']);
+}
+
+ini harusnya salah
